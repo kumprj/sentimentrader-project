67ed1da44f3af87baa0af54733269633a923bf38
Robert Kump <kumprj@miamioh.edu>
2019-10-24 20:23:45 -0400

    preparing code for release

---
+++ sentimentrader_backtest/SentimentTraderBacktest.py
@@ -0,0 +1,315 @@
+import psycopg2
+import csv
+import time
+from datetime import date
+import os
+import sys
+# from itertools import izip as izip
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from pyvirtualdisplay import Display
+import boto3
+from botocore.exceptions import ClientError
+from credentials import loadCredentials
+
+# Global Variables to specify High and Low Extreme Sentiment
+LOW_EXTREME = '20'
+HIGH_EXTREME = '85'
+
+def main():
+    query_database()
+
+
+
+def query_database():
+    credentials = loadCredentials()
+    database_user = credentials["database"]["username"]
+    database_password = credentials["database"]["password"]
+    database_db = credentials["database"]["database"]
+    database_host = credentials["database"]["host"]
+    database_port = credentials["database"]["port"]
+
+    connection = psycopg2.connect(user = database_user,
+                            password = database_password,
+                            host = database_host,
+                            port = database_port,
+                            database = database_db)
+
+    cursor = connection.cursor()
+    test_extreme = HIGH_EXTREME
+    select_query = '''SELECT name, indicator, last_close
+                    FROM "dailyIndicatorSTG"
+                    WHERE name LIKE '%%Optix%%'
+                    AND CAST(last_close AS float)  > %s
+                        '''  
+    
+    cursor.execute(select_query, [test_extreme])
+    positiveOptixIndicatorQueryList = cursor.fetchall() # Add all of the results to a list. Creates list of tuples
+
+    test_extreme = LOW_EXTREME
+    select_query = '''SELECT name, indicator, last_close
+                    FROM "dailyIndicatorSTG"
+                    WHERE name LIKE '%%Optix%%'
+                    AND CAST(last_close AS float)  < %s
+                        '''
+   
+    cursor.execute(select_query, [test_extreme])
+    negativeOptixIndicatorQueryList = cursor.fetchall()
+
+    # Combine our positive and negative lists.
+    optixIndicatorQueryList = positiveOptixIndicatorQueryList + negativeOptixIndicatorQueryList
+
+    # Gets each element from the tuples the database returns
+    optixNameList = [x[0] for x in optixIndicatorQueryList]
+    indicatorNameList = [x[1] for x in optixIndicatorQueryList]
+    lastCloseList = [x[2] for x in optixIndicatorQueryList]
+
+    # Pass our lists to generate the list of backtests
+    generate_list_of_backtests(optixNameList, indicatorNameList, lastCloseList)
+
+
+
+# Loop through List of URLs, insert results into postgres database.
+def run_backtest(webPageList):
+
+    # User Information
+    credentials = loadCredentials()
+    sentiment_user = credentials["sentimentrader"]["username"]
+    sentiment_password = credentials["sentimentrader"]["password"]
+
+    username = sentiment_user
+    password = sentiment_password
+
+    # Chrome Driver is required in path or specify path within argument (which chromedriver to find
+    # Initiate driver and browser instance
+    display = Display(visible=0, size=(800, 800))  
+    display.start()
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+    # To run as root, use above 3 lines. To run as regular user, the below is fine.
+    # driver = webdriver.Chrome('/usr/bin/chromedriver')
+    driver.get('https://sentimentrader.com/')
+
+    # Login to sentimentrader.com
+    username_field = driver.find_element_by_id('username')
+    password_field = driver.find_element_by_id('password')
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    print('Successfully logged in')
+    
+
+    for backtestUrl in webPageList:
+
+        # Goes to the sentinmentrader.com backtest url from the webPageList.
+        driver.get(backtestUrl)
+        time.sleep(7)
+        
+        # Clicks the back-test url button to run the backtest.
+        driver.find_element_by_xpath("//span[contains(@class,'run-text')]").click()
+        time.sleep(12)
+
+        # Try catch to see if our backtest actually has data, or if there's no historical trades. If no historical trades, continue.
+        try:
+            isInvalid = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[2]/div/p").get_attribute("textContent").strip() # Checks for text that there were no trades.
+            failedTestSymbol = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[1]/div[1]/div/div/div/h3").get_attribute("textContent").strip() # Symbol
+            failedIndicatorSymbol = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[1]/div[2]/div/div/div/h3").get_attribute("textContent").strip() # Indicator
+            failedIndicatorLevel = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[1]/div[2]/div/div/div/h4[2]").get_attribute("textContent").strip() # Indicator Level
+            failedLookbackPeriod = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[1]/div[3]/div/div/div/h3").get_attribute("textContent").strip()             
+            failedObservationPeriod = driver.find_element_by_xpath("//div[@id='backtest-results']/div/div[2]/div[1]/div[4]/div/div/div/h3").get_attribute("textContent").strip() 
+            
+            print('''%s 
+                Symbol %s
+                Indicator %s
+                failedIndicatorLevel %s
+                failedLookbackPeriod %s
+                failedObservationPeriod %s
+                ''' % (isInvalid, failedTestSymbol, failedIndicatorSymbol, failedIndicatorLevel, failedLookbackPeriod, failedObservationPeriod))            
+            continue
+        except Exception:
+            pass
+
+        print('Backtest is a valid test with historical data. Proceeding..')     
+        # Get values of Backtest Summary section. TODO: Can probably get these from the variables in generate_list_of_backtests(), but it works.
+        # Column 1
+        currentTestSymbol = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[1]/div/div/div/h3").get_attribute("textContent").strip() # Symbol
+        print('Running backtest of %s' % (currentTestSymbol))
+        currentTestIndexFilter = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[1]/div/div/div/h4[1]").get_attribute("textContent").strip() # Index Filter
+        currentTestIndexFilterSwap = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[1]/div/div/div/h4[2]").get_attribute("textContent").strip() # Index Filter Swap
+        currentTestIndexMASlope = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[1]/div/div/div/h4[3]").get_attribute("textContent").strip() # Index MA Slope
+
+        # Column 2
+        currentTestIndicator = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[2]/div/div/div/h3").get_attribute("textContent").strip() # Indicator
+        currentTestIndicatorSmoothing = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[2]/div/div/div/h4[1]").get_attribute("textContent").strip() # Indicator Smoothing
+        currentTestIndicatorLevel = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[2]/div/div/div/h4[2]").get_attribute("textContent").strip() # Indicator Level
+        
+        # Column 3
+        currentTestLookbackPeriod = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[3]/div/div/div/h3").get_attribute("textContent").strip() # Lookback Period
+        currentTestStartdate = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[3]/div/div/div/h4[1]").get_attribute("textContent").strip() # Start Date
+        currentTestEndDate = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[3]/div/div/div/h4[2]").get_attribute("textContent").strip() # End Date
+
+        # Column 4
+        currentTestObservationPeriod = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[4]/div/div/div/h3").get_attribute("textContent").strip() # Observation Period
+        currentTestOverlapping = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[4]/div/div/div/h4[1]").get_attribute("textContent").strip() # Exclude Overlapping
+        currentTestMarketEnvironment = driver.find_element_by_xpath("//div[@id='summary-body']/div/div[4]/div/div/div/h4[2]").get_attribute("textContent").strip() # Market Environment
+
+        # Backtest Statistics Table
+        # Row 1
+        currentTotalReturn = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[1]/div[1]/div[2]").get_attribute("textContent").strip() # Total Return
+        currentAvgReturn = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[1]/div[2]/div[2]").get_attribute("textContent").strip() # Average Return
+        currentZSCore = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[1]/div[3]/div[2]").get_attribute("textContent").strip() # Z-Score
+        currentBuyHoldReturn = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[1]/div[4]/div[2]").get_attribute("textContent").strip() # Buy & Hold Return
+
+        # Row 2
+        currentWinRatePercent = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[2]/div[1]/div[2]").get_attribute("textContent").strip() # Win Rate
+        currentAvgWin = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[2]/div[2]/div[2]").get_attribute("textContent").strip() # Average Win %
+        currentAvgLoss = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[2]/div[3]/div[2]").get_attribute("textContent").strip() # Average Loss %
+        currentMaxRisk = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[2]/div[4]/div[2]").get_attribute("textContent").strip() # Max Risk
+
+        # Row 3
+        currentTotalTrades = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[3]/div[1]/div[2]").get_attribute("textContent").strip() # Total Trades
+        currentTotalPositive = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[3]/div[2]/div[2]").get_attribute("textContent").strip() # Total Positive
+        currentTotalNegative = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[3]/div[3]/div[2]").get_attribute("textContent").strip() # Total Negative
+        currentTimeInMarket = driver.find_element_by_xpath("//div[@id='stat_tab']/div[1]/div[3]/div[4]/div[2]").get_attribute("textContent").strip() # % Time In Market
+        todaysDate = date.today()
+        todaysDate = todaysDate.strftime("%m/%d/%y")
+
+        credentials = loadCredentials()
+        database_user = credentials["database"]["username"]
+        database_password = credentials["database"]["password"]
+        database_db = credentials["database"]["database"]
+        database_host = credentials["database"]["host"]
+        database_port = credentials["database"]["port"]
+
+        connection = psycopg2.connect(user = database_user,
+                                password = database_password,
+                                host = database_host,
+                                port = database_port,
+                                database = database_db)
+
+        current_test_data = (currentTestSymbol, currentTestIndicator, currentTestLookbackPeriod, currentTestObservationPeriod, currentTestIndexFilter,
+                        currentTestIndicatorSmoothing, currentTestStartdate, currentTestEndDate, currentTestOverlapping, currentTestIndexFilterSwap, currentTestIndicatorLevel,
+                        currentTestMarketEnvironment, currentTestIndexMASlope, currentTotalReturn, currentAvgReturn, currentZSCore, currentBuyHoldReturn, currentWinRatePercent,
+                        currentAvgWin, currentAvgLoss, currentMaxRisk, currentTotalTrades, currentTotalPositive, currentTotalNegative, 
+                        currentTimeInMarket, todaysDate, currentTotalReturn, currentAvgReturn, currentZSCore, currentBuyHoldReturn, currentWinRatePercent, currentAvgWin,
+                        currentAvgLoss, currentMaxRisk, currentTotalTrades, currentTotalPositive, currentTotalNegative, currentTimeInMarket)
+        sql_insert = '''insert into "backtest_results" (symbol, indicator, lookback_period, observation_period, index_filter, indicator_smoothing, 
+                        test_start_date, test_end_date, exclude_overlapping, index_filter_swap, indicator_level, market_environment, 
+                        index_ma_slope, total_return, avg_return, z_score, buy_hold_return, win_rate, avg_win,
+                        avg_loss, max_risk, total_trades, total_pos, total_neg, time_in_mkt, todays_date)                    
+                        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
+                        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT (indicator, observation_period, todays_date)
+                        DO UPDATE SET total_return = %s,
+                        avg_return = %s,
+                        z_score = %s,
+                        buy_hold_return = %s,
+                        win_rate = %s,
+                        avg_win = %s,
+                        avg_loss = %s,
+                        max_risk = %s,
+                        total_trades = %s,
+                        total_pos = %s,
+                        total_neg = %s,
+                        time_in_mkt = %s;
+                        '''
+        cursor = connection.cursor()
+        cursor.execute(sql_insert, current_test_data)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        print('Backtest Table updated for %s for period %s' % (currentTestIndicator, currentTestObservationPeriod))
+
+    # End backtest loop
+    # Close driver for chrome.
+    driver.close()
+    
+# end run_backtest
+
+# Get low and high extremes to backtest.
+def get_closest_optix_extreme(lastClose):
+    lastClose = float(lastClose)
+    if lastClose < float(LOW_EXTREME):
+        return LOW_EXTREME
+    return HIGH_EXTREME
+# end get_closest_optix_extreme
+
+# Generate our list of backtests to loop through. TODO: Default values supplied, may need customization options.
+def generate_list_of_backtests(optixNameList, indicatorNameList, lastCloseList):
+    # Sample Optix url: 
+    # https://sentimentrader.com/users/backtest/?indicator_smoothing=0&index_filter=0&market_environment=0&index_ma_slope=0&lookback_period=16&observation_value=3&observation_period=4&indicator_condition=3&indicator_level=80&index=SPY&indicator=SPY+Optix 
+    
+    # Dynamic URL    
+    baseUrl = 'https://sentimentrader.com/users/backtest/?indicator_smoothing=indicatorSmoothingValue&index_filter=indexFilterValue&market_environment=marketEnvironmentValue&index_ma_slope=maSlopeValue&lookback_period=lookbackPeriodValue&observation_value=observationValue&observation_period=observationPeriodValue&indicator_condition=indicatorConditionValue&indicator_level=indicatorLevelValue&index=indexValue&indicator=indicatorValue'
+    webPageList = []
+    listOfObservationValues = ['1', '3', '6', '9', '12'] # Months back to test.
+    indicatorSmoothingValue = '0'
+    indexFilterValue = '0'
+    marketEnvironmentValue = '0'
+    maSlopeValue = '0'
+    lookbackPeriodValue = '16' # All History    
+    observationPeriodValue = '4' # 4 is months
+    indicatorConditionValue = '3' # 3 is above
+    indicatorLevelValue = '0' # 20 and 80 are what we identified as extremes, and these are set in the get_closest_optix_extreme.
+    # indexValue = 'SPY' # ETF/Commodity/index to query <-- I believe this is unused/not needed.
+    # observationValue = '1' # How many months to look back <-- I believe this is unused/not needed.
+    # indicatorValue = 'SPY+Optix' # Optix value <-- I believe this is unused/not needed.
+
+    # for optixName, indicatorName, lastClose in izip(optixNameList, indicatorNameList, lastCloseList): # Python 2 code
+    for optixName, indicatorName, lastClose in zip(optixNameList, indicatorNameList, lastCloseList):
+    # for optixName, indicatorName in izip(optixNameList, indicatorNameList):
+        for lengthOfTime in listOfObservationValues:
+
+            currentBackTestUrl = baseUrl # Make a local copy of the base URL for manipulation.
+
+            optixName = optixName.replace(" ", "+") # Replace "SPY Optix with SPY+Optix"                    
+            currentBackTestUrl = currentBackTestUrl.replace('indicatorValue', optixName)
+
+            indicatorName = indicatorName.replace('etf_','') # Indicator is entered into db as etf_indexname i.e. etf_lqd, so we split that off.
+            indicatorName = indicatorName.replace('po_','') # Indicator is entered into db as po_indexname i.e. po_xx, so we split that off.
+            indicatorName = indicatorName.replace('country_','') # Indicator is entered into db as country_indexname i.e. country_xx, so we split that off.
+            indicatorName = indicatorName.replace('model_score_','') # Indicator is entered into db as model_score_indexname i.e. model_score_xx, so we split that off.
+            indicatorName = indicatorName.upper()
+
+            if indicatorName == 'XBT': indicatorName = 'BITCOIN' # Edge case where the indicator value does not match the sentimentrader url.
+            if indicatorName == 'BITCOIN' and lengthOfTime == '12':  # Edge case where the length of time value for bitcoin + 12 months causes an error.
+                continue
+
+            currentBackTestUrl = currentBackTestUrl.replace('indexValue', indicatorName) # Append the index/commodity/etf that we are backtesting.
+            
+            # Set default
+            currentBackTestUrl = currentBackTestUrl.replace('observationValue', lengthOfTime)
+            currentBackTestUrl = currentBackTestUrl.replace('indicatorSmoothingValue', indicatorSmoothingValue)
+            currentBackTestUrl = currentBackTestUrl.replace('indexFilterValue', indexFilterValue)
+            currentBackTestUrl = currentBackTestUrl.replace('marketEnvironmentValue', marketEnvironmentValue)
+            currentBackTestUrl = currentBackTestUrl.replace('maSlopeValue', maSlopeValue)
+            currentBackTestUrl = currentBackTestUrl.replace('lookbackPeriodValue', lookbackPeriodValue)
+            currentBackTestUrl = currentBackTestUrl.replace('observationPeriodValue', observationPeriodValue)
+            
+
+            indicatorLevelValue = get_closest_optix_extreme(lastClose) # Check if we're at a low extreme or high extreme.
+            currentBackTestUrl = currentBackTestUrl.replace('indicatorLevelValue', indicatorLevelValue) # Set the value low or high extreme.
+            
+            if indicatorLevelValue == LOW_EXTREME:
+                indicatorConditionValue = '4'
+                currentBackTestUrl = currentBackTestUrl.replace('indicatorConditionValue', indicatorConditionValue)
+            else:
+                currentBackTestUrl = currentBackTestUrl.replace('indicatorConditionValue', indicatorConditionValue)                   
+
+            # Append URL to our list to backtest for each period of time.            
+            webPageList.append(currentBackTestUrl)  
+
+        # end time period back tests of 1/3/6/9/12 months          
+
+    # end for loop of optix name, indicator, last close
+
+    run_backtest(webPageList)
+# end generate_list_of_backtests
+
+
+if __name__ == "__main__":
+    main()
---
+++ sentimentrader_indicator_loading/SentimentTraderDailyIndicator.py
@@ -0,0 +1,99 @@
+import psycopg2
+import csv
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from pyvirtualdisplay import Display
+from credentials import loadCredentials
+
+# User Information
+credentials = loadCredentials()
+sentiment_user = credentials["sentimentrader"]["username"]
+sentiment_password = credentials["sentimentrader"]["password"]
+
+username = sentiment_user
+password = sentiment_password
+
+
+# Chrome Driver is required in path or specify path within argument (which chromedriver to find
+# Initiate driver and browser instance
+display = Display(visible=0, size=(800, 800))  
+display.start()
+# driver = webdriver.Chrome('/usr/bin/chromedriver') # No options webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless') # Unnecessary in this case but a common chrome option to add.
+driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+driver.get('https://sentimentrader.com/')
+
+# Login and download
+username_field = driver.find_element_by_id('username')
+password_field = driver.find_element_by_id('password')
+
+username_field.send_keys(username)
+password_field.send_keys(password)
+password_field.send_keys(Keys.RETURN)
+
+driver.get('https://sentimentrader.com/users/ws/todaysentiment')
+time.sleep(10)
+driver.close()
+print('Sentiment report downloaded')
+
+# remove key column for import
+# Linux Image default downloads dir: /root/Downloads/
+with open('/root/Downloads/sentimenTrader_Daily_Sentiment_Data.csv', 'r') as fin, open('/root/Downloads/sentimenTrader_Daily_Sentiment_DataE.csv', 'w') as fout:
+    reader = csv.reader(fin) 
+    reader.next() #skip header
+    writer = csv.writer(fout)
+    for row in reader:
+        writer.writerow(row[1:])
+        
+# write new data to stage table CHECK insert statment
+database_user = credentials["database"]["username"]
+database_password = credentials["database"]["password"]
+database_db = credentials["database"]["database"]
+database_host = credentials["database"]["host"]
+database_port = credentials["database"]["port"]
+
+connection = psycopg2.connect(user = database_user,
+                        password = database_password,
+                        host = database_host,
+                        port = database_port,
+                        database = database_db)
+
+
+with open ('/root/Downloads/sentimenTrader_Daily_Sentiment_DataE.csv', 'r') as f:
+    cursor = connection.cursor()
+    cursor.execute('delete from "dailyIndicatorSTG";')
+    cursor.copy_from(f,'"dailyIndicatorSTG"', sep=',')
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print('sent to stagetable')
+
+# Update prd table with updates made today 
+connection = psycopg2.connect(user = database_user,
+                        password = database_password,
+                        host = database_host,
+                        port = database_port,
+                        database = database_db)
+cursor = connection.cursor()
+cursor.execute('''
+                   insert into indicatorprd  ("indicator","value","date")
+                    select S."indicator",
+                              S."last_close",
+                              S."update_date" 
+                    from public."dailyIndicatorSTG" S
+                    left join (select "indicator", max("date") as "date" from public.indicatorprd group by "indicator") P
+                    on P."indicator"=S."indicator"
+                    where P."date"!=S.update_date
+                    ''')
+connection.commit()
+cursor.close()
+connection.close()
+print('PRD table Updated')
+
+# Removed files
+os.remove("/root/Downloads/sentimenTrader_Daily_Sentiment_Data.csv")
+os.remove("/root/Downloads/sentimenTrader_Daily_Sentiment_DataE.csv")
