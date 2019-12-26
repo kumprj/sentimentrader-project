ebf69b20ee26b66b77cb0efecc1d61eed53aec1a
Robert Kump <kumprj@miamioh.edu>
2019-12-25 22:01:44 -0500

    pushing to live

---
+++ email_scripts/generate_email.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import json
+import requests
+import time
+from datetime import date
+
+
+def main():
+    generateListToemail()
+
+def generateListToEmail():
+    url = 'https://4w629k6x07.execute-api.us-east-2.amazonaws.com/dev/backtest_results_10ma'
+    todaysDate = date.today()
+    todaysDate = todaysDate.strftime("%m/%d/%y")
+    todaysDate = todaysDate.replace('/','_')
+
+    resp = requests.get(f'{url}/{todaysDate}')
+    jsonData = []
+
+    if (resp.ok):
+        jsonData = json.loads(resp.content)
+
+    masterList = []
+    stringListToEmail = []
+    for backtestEntry in jsonData:
+        # print(backtestEntry)
+        currentWinRate = backtestEntry['win_rate']
+        currentWinRate = currentWinRate.replace('%','')
+        currentWinRateAsInt = int(currentWinRate)
+        if (currentWinRateAsInt > 20 and currentWinRateAsInt < 80):
+            continue
+
+        # If we get this far, the win rates are really good or really bad, so append it to the list.
+        masterList.append(backtestEntry)
+
+    for entry in masterList:  
+        stringToEmail = f'''Ticker {entry["symbol"]} has a win rate of {entry["win_rate"]} for time period {entry["observation_period"]}. The median return is {entry['avg_return']}, avg win is {entry['avg_win']} and avg loss is {entry['avg_loss']} over a total of {entry['total_trades']} trades.'''
+        stringListToEmail.append(stringToEmail)
+    
+    return stringListToEmail
+
+
+if __name__ == '__main__':
+    main()
---
+++ email_scripts/send_email.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import csv
+import time
+from datetime import date
+import os
+import sys
+import boto3
+from botocore.exceptions import ClientError
+from generate_email import generateListToEmail
+
+
+def main():
+    send_email()
+
+
+def parseTickers(listOfTickers):
+    resultingList = []
+    splitNewLineList = listOfTickers.splitlines()
+
+    # wrap lines in p tags
+    for item in splitNewLineList:
+        # print(f'i is {i}')
+        resultingList.append('<p>'+item+'</p>')
+    
+    return resultingList
+
+def send_email():
+    stringsToEmail =  generateListToEmail()
+    stringsOnSeparateLines = "\n".join(stringsToEmail)
+
+    SENDER = "..."
+    RECIPIENT1 = "..."
+    RECIPIENT2 = "..."
+    AWS_REGION = "us-east-1"
+    SUBJECT = "10 Day MA Daily Extreme Update"
+
+    # non-HTML email clients body.
+    BODY_TEXT = (f"{stringsOnSeparateLines}")
+    
+    # HTML body
+    BODY_HTML = f"""<html>
+    <head></head>
+    <body>
+        {parseTickers(stringsOnSeparateLines)}
+    </body>
+    </html>
+        """
+
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify the region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT1,RECIPIENT2
+                ],
+                
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML, # replace with contents to email
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    # Handle errors
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+
+
+if __name__ == '__main__':
+    main()
